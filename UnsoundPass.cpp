8a3cc152e1612c9efc75164a36404d01551c6ad5
Bobby King <jking31cs@gmail.com>
2014-03-09 16:58:21 -0400

    Setup the unsound test, working on source level info.

---
+++ UnsoundPass.cpp
@@ -0,0 +1,42 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/InstVisitor.h"
+#include "llvm/Analysis/CFG.h"
+#include <set>
+#include <list>
+using namespace llvm;
+
+namespace {
+
+    struct UnsoundPass : public FunctionPass {
+        static char ID;
+        UnsoundPass() : FunctionPass(ID) {}
+        
+        virtual bool runOnFunction(Function &F) {
+            std::set<Value*> allocated;
+            std::set<Value*> defined;
+            for (Function::iterator fit = F.begin(); fit != F.end(); fit++) {
+                for (BasicBlock::iterator bbit = fit->begin(); bbit != fit->end(); bbit++) {
+                    if (AllocaInst *inst = dyn_cast<AllocaInst>(bbit)) {
+                        allocated.insert(dyn_cast<Value>(inst));
+                    }
+                    if (StoreInst *inst = dyn_cast<StoreInst>(bbit)) {
+                        defined.insert(inst->getPointerOperand());
+                    }
+                    if (LoadInst *inst = dyn_cast<LoadInst>(bbit)) {
+                        if (defined.find(dyn_cast<Value>(inst)) == defined.end()) {
+                            errs() << "Uninitialized variable found " << inst->getPointerOperand()->getName() << '\n';
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        
+    };
+    
+    char UnsoundPass::ID = 0;
+    static RegisterPass<UnsoundPass> X("unsound-pass", "Detects Unsound calls", false, false);
+}
