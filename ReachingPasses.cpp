4a916118b66662e624ec4c22f4449b47e98e9e18
Bobby King <jking31cs@gmail.com>
2014-03-08 20:12:09 -0500

    Gen and Kill Sets made

--- ReachingPasses.cpp
+++ ReachingPasses.cpp
@@ -2,35 +2,78 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Instructions.h"
-#include "llvm/InstVisitor.h"
+#include "llvm/IR/InstVisitor.h"
+#include <set>
+#include <list>
 using namespace llvm;
 
 namespace {
 
-    struct CustomInstVisitor : public InstVisitor<CustomInstVisitor> {
-        CustomInstVisitor() : InstVisitor() {}
-        
-        void visitStoreInst(StoreInst &i) {
-            errs() << "Store Inst Found: " << i.getName() << '\n';
-        }
-
-        void visitAllocaInst(AllocaInst &i) {
-            errs() << "Allocate Inst Found: " << i.getName() << '\n';
-        }
-
-        void visitInstruction(Instruction &i) {
-            errs() << "Analyzing Instruction: " << i.getName() << '\n';
-        }
+    class GenAndKillSets {
+        public:
+            std::set<StoreInst *> gen;
+            std::set<StoreInst *> kill;
     };
-    
+
+    class BBGenAndKillSets {
+        public:
+            BasicBlock *bb;
+            std::set<StoreInst *> gen;
+            std::set<StoreInst *> kill;
+            BBGenAndKillSets(BasicBlock *val) { bb = val; }
+            std::string genString() {
+                std::string ret = "Gen Set of BB:" + bb->getName().str() + '\n';
+                for (std::set<StoreInst *>::iterator it = gen.begin(); it != gen.end(); it++) {
+                    StoreInst *inst = *it;
+                    ret += '\t' + inst->getPointerOperand()->getName().str() + '\n';
+                }
+                return ret;
+            }
+    };
+
     struct NaivePass : public FunctionPass {
         static char ID;
         NaivePass() : FunctionPass(ID) {}
         
         virtual bool runOnFunction(Function &F) {
-            CustomInstVisitor visitor;
-            visitor.visit(F); 
+            std::list<BBGenAndKillSets> listOfSets;
+            for (Function::iterator f_it = F.begin(); f_it != F.end(); ++f_it) {
+                BBGenAndKillSets bbSets = createBBGenAndKillSets(F, f_it); 
+                errs() << bbSets.genString();
+            }
             return false;
+        }
+
+        BBGenAndKillSets createBBGenAndKillSets(Function &F, BasicBlock *f_it) {
+            BBGenAndKillSets bbSets(f_it);
+            for (BasicBlock::iterator bb_it = f_it->begin(); bb_it != f_it->end(); bb_it++) {
+                if (StoreInst *inst = dyn_cast<StoreInst>(bb_it)) {
+                    Value *var = inst->getPointerOperand();
+                    GenAndKillSets sets;
+                    sets.gen.insert(inst);
+                    for (Function::iterator f2_it = F.begin(); f2_it != F.end(); ++f2_it) { 
+                        for (BasicBlock::iterator bb2_it = f2_it->begin(); 
+                                bb2_it != f2_it->end(); bb2_it++) {
+                            if (bb2_it == bb_it) {
+                                continue;
+                            }
+
+                            if (StoreInst *inst2 = dyn_cast<StoreInst>(bb2_it)) {
+                                if (inst2->getPointerOperand() == var) {
+                                    sets.kill.insert(inst2);
+                                }
+                            }
+                        } 
+                    }
+                    for (std::set<StoreInst *>::iterator gen = sets.gen.begin(); gen != sets.gen.end(); gen++) {
+                        bbSets.gen.insert(*gen);
+                    }
+                    for (std::set<StoreInst *>::iterator kill = sets.kill.begin(); kill != sets.kill.end(); kill++) {
+                        bbSets.kill.insert(*kill);
+                    }
+                }
+            }
+            return bbSets;
         }
         
     };
